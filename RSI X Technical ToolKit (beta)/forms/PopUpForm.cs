3fd9fba909e0d8f17b4f4cb80e790d05acc7e717
HarryP0ster <garryplakat@gmail.com>
2022-01-20 15:05:29 +0800

    .

--- RSI X Technical ToolKit (beta)/forms/PopUpForm.cs
+++ RSI X Technical ToolKit (beta)/forms/PopUpForm.cs
@@ -130,6 +130,11 @@ namespace RSI_X_Desktop.forms
             AgoraObject.Rtc.EnableVideo();
 
             SetWndRegion();
+            TableMisc.Hide();
+            TableGeneral.Show();
+            MainLayout.ColumnStyles[0].Width = 100;
+            MainLayout.ColumnStyles[1].Width = 0;
+
             oldVolumeIn = RecordersManager.GetDeviceVolume();
             trackBarSoundIn.Value = oldVolumeIn;
             trackBarSoundOut.Value = oldVolumeOut;
